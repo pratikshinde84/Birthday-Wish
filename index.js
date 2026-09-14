1ceba8bc02c75906ee4f66a3e36074b7447fa387
Pratik <pratik216029@gmail.com>
2026-09-14 23:31:30 +0530

    done

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const user={
 console.log(user.age);
 console.log(Object.keys(user));
 console.log(Object.values(user));
+console.log(Object.seal(user));
+
+let ar=[1,2,3,4,5];
+console.log(ar.sort((a,b)=>a-b));
---
+++ janhavi.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int add(int a,int b){
+    int c=a+b;
+    
+}
+
+int main(){
+    add(5,6);
+    return 0;
+}
