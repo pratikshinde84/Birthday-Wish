38548a3b4fb3a9e8b86aedf9d0a9be06d7f10dbe
Pratik <pratik216029@gmail.com>
2026-09-14 17:48:10 +0530

    done 2

---
+++ index.js
@@ -0,0 +1,5 @@
+let name = prompt("Enter your name:");
+
+console.log(name);
+alert("Welcome to my website!");
+let result = confirm("Do you want to delete this file?");console.error("Something went wrong!");
