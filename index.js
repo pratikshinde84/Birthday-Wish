117c51bf4c50154a31102b1b8f50ef6b7a450a0e
Pratik <pratik216029@gmail.com>
2026-09-15 11:39:54 +0530

    done

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const a=await fetch(
-    "https://typicode.com"
-)
-console.log(await a.json())
+fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        return response.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error("Fetch error:", error));
