1ebc111197584a40abca09d60295a7b8a55fb108
Pratik <pratik216029@gmail.com>
2026-09-15 11:36:05 +0530

    done

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-const user={
-    age:20,
-    name:`John Doe`
-}
-console.log(user.age);
-console.log(Object.keys(user));
-console.log(Object.values(user));
-console.log(Object.seal(user));
-
-let ar=[1,2,3,4,5];
-console.log(ar.sort((a,b)=>a-b));
+const a=await fetch(
+    "https://typicode.com"
+)
+console.log(await a.json())
