3def635d0d1ffb74b54a77b1d16d48dbf830e3a9
Pratik <pratik216029@gmail.com>
2026-09-17 02:48:58 +0530

    done

---
+++ Methods.java
@@ -0,0 +1,14 @@
+class Methods{
+    int printName(int a,int b){
+        System.out.println("Two parammetters");
+        return a+b;
+    }
+    int printName(int a,int b,int c){
+        System.out.println("Three parameters");
+        return a+b+c;
+    }
+    public static void main(String args[]){
+        Methods m=new Methods();
+        m.printName(3,5,3);
+    }
+}
